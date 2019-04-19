5909b68e4ac27762bb806912389c79d0fcf8e1b7
Rifat Hossain <admin@rhossain.me>
2019-04-20 05:46:54 +0600

    mixitup added

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,11 @@ import "../scss/bundle.scss";
 
 
 
-window.$ = window.jQuery =  require('jquery');
+window.$ = window.jQuery = require('jquery');
 require('popper.js');
 require('bootstrap');
 require('@fortawesome/fontawesome-free/js/all');
+let mixitup = require('mixitup');
+
+// mixitContainer
+var mixer = mixitup('.mixitContainer');
